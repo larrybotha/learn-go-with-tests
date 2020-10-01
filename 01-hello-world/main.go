e4def8548cc625f6c5ecbbd76eb8abad5878d881
Larry Botha <larry@fixate.it>
2020-09-30 22:32:41 -0500

    update function to accept parameter

--- 01-hello-world/main.go
+++ 01-hello-world/main.go
@@ -2,10 +2,13 @@ package main
 
 import "fmt"
 
-func Hello() string {
-	return "Hello, world"
+/**
+ * move behaviiour out of 'main' so that it can be tested independntly
+ */
+func Hello(name string) string {
+	return "Hello, " + name
 }
 
 func main() {
-	fmt.Println(Hello())
+	fmt.Println(Hello("world"))
 }
