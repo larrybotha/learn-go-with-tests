237bdf090e61ff6e2c9de2d8d9fcdb16df5d44f0
Larry Botha <larry@fixate.it>
2020-10-08 21:10:02 -0500

    install spew

--- 03-iteration/go.mod
+++ 03-iteration/go.mod
@@ -1,3 +1,5 @@
 module github.com/larryotha/learn-go-with-tests/03-iteration
 
 go 1.15
+
+require github.com/davecgh/go-spew v1.1.1
--- 03-iteration/iteration.go
+++ 03-iteration/iteration.go
@@ -1,11 +1,15 @@
 package iteration
 
+//import "github.com/davecgh/go-spew/spew"
+
 func Repeat(value string, repeats int) string {
 	var result string
 
 	for n := 0; n < repeats; n++ {
 		result = result + value
 	}
+
+	//spew.Dump(result)
 
 	return result
 }
