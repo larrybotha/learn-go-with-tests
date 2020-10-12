ae2431c3a249e14f8cac46c665a89efd47cf41f0
Larry Botha <larry@fixate.it>
2020-10-11 22:25:28 -0500

    add tests and implementations

--- 04-arrays-and-slices/sum.go
+++ 04-arrays-and-slices/sum.go
@@ -25,3 +25,31 @@ func Sum(xs []int) int {
 
 	return sum
 }
+
+// SumAllImpure mutates a slice by reassigning indexes on each iteration
+// It uses `makr` to create a slice with a capacity upfront
+func SumAllImpure(xxs ...[]int) (sums []int) {
+	numArgs := len(xxs)
+	// slices have a capacity. If we ar assigning values to explicit indexes, we
+	// need to know upfront the capacity for the slice. We can then create the
+	// slice using `make`
+	sums = make([]int, numArgs)
+
+	for i, xs := range xxs {
+		sums[i] = Sum(xs)
+	}
+
+	return
+}
+
+// SumAll will not mutate sums, but will instead reassign the variable on
+// each iteration using the `assign` built-in function
+func SumAll(xxs ...[]int) (sums []int) {
+	sums = []int{}
+
+	for _, xs := range xxs {
+		sums = append(sums, Sum(xs))
+	}
+
+	return
+}
--- 04-arrays-and-slices/sum_test.go
+++ 04-arrays-and-slices/sum_test.go
@@ -2,6 +2,7 @@ package sum
 
 import (
 	"fmt"
+	"reflect"
 	"testing"
 )
 
@@ -36,4 +37,28 @@ func ExampleSum() {
 	result := Sum([]int{1, 2, 3})
 	fmt.Println(result)
 	// Output: 6
+}
+
+func TestSumAllImpure(t *testing.T) {
+	xs1 := []int{1, 1, 1}
+	xs2 := []int{2, 2, 2}
+
+	actual := SumAllImpure(xs1, xs2)
+	expected := []int{3, 6}
+
+	if !reflect.DeepEqual(actual, expected) {
+		t.Errorf("expected %v to be %v", actual, expected)
+	}
+}
+
+func TestSumAllPure(t *testing.T) {
+	xs1 := []int{1, 1, 1}
+	xs2 := []int{2, 2, 2}
+
+	actual := SumAll(xs1, xs2)
+	expected := []int{3, 6}
+
+	if !reflect.DeepEqual(actual, expected) {
+		t.Errorf("expected %v to be %v", actual, expected)
+	}
 }
