821fe67872cb2f4d51ab95f62eafa6ea12e89cd3
Larry Botha <larry@fixate.it>
2020-10-16 22:37:33 -0500

    add triangle

--- 05-structs-methods-interfaces/shapes.go
+++ 05-structs-methods-interfaces/shapes.go
@@ -15,6 +15,15 @@ type Circle struct {
 	Radius float64
 }
 
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+func (t Triangle) Area() float64 {
+	return t.Base * t.Height * .2
+}
+
 func (r Rectangle) Area() float64 {
 	return r.Width * r.Height
 }
