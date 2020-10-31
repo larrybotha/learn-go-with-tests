f3eacd3645aac2b2183c961b79514cb4ce7cf77a
Larry Botha <larry@fixate.it>
2020-10-31 18:32:34 -0500

    add first test

---
+++ 08-dependency-injection/di_test.go
@@ -0,0 +1,21 @@
+package di
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestGreet(t *testing.T) {
+	t.Run("prints a value", func(t *testing.T) {
+		buffer := bytes.Buffer{}
+
+		Greet(&buffer, "Joe")
+
+		got := buffer.String()
+		want := "Hello, Joe"
+
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
+}
