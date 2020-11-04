0c9b9d90ff7219c76eaf8431c729796c0cdf3eee
Larry Botha <larry@fixate.it>
2020-11-03 21:55:40 -0500

    add notes

--- 08-dependency-injection/di.go
+++ 08-dependency-injection/di.go
@@ -6,15 +6,34 @@ import (
 	"net/http"
 )
 
+/*
+By accepting a value that implements Writer
+
+This allows for two things:
+
+- the call site can provide whatever writer it wants
+- we can now test Greet by providing a writer that doesn't write to stdout
+*/
 func Greet(writer io.Writer, name string) {
 	fmt.Fprintf(writer, "Hello, %s", name)
 }
 
+/*
+http.ResponseWriter implements io.Writer, allowing us to pass its variable through
+to Greet
+
+Request handlers in Go take 2 arguments, similarly to Node, except that in Node
+the order is switched - handler(req, res)
+*/
 func MyGreeterHandler(w http.ResponseWriter, r *http.Request) {
 	Greet(w, "World\n")
 }
 
 func main() {
+	/*
+		In Go, one can listen and serve requests in a single call, as opposed to in Node
+		where a server first needs to be created, and then listened to
+	*/
 	http.ListenAndServe(
 		":3000",
 		http.HandlerFunc(MyGreeterHandler),
