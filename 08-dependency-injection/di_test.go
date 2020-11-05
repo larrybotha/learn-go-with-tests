97350f7d1c6a36ffa1cb71a0db06d177eea04eb3
Larry Botha <larry@fixate.it>
2020-11-04 23:56:02 -0500

    add comments to tests

--- 08-dependency-injection/di_test.go
+++ 08-dependency-injection/di_test.go
@@ -7,8 +7,10 @@ import (
 
 func TestGreet(t *testing.T) {
 	t.Run("prints a value", func(t *testing.T) {
+		// bytes.Buffer implements Writer
 		buffer := bytes.Buffer{}
 
+		// why is buffer passed in with an &?
 		Greet(&buffer, "Joe")
 
 		got := buffer.String()
