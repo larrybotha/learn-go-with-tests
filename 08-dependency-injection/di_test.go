81492fda4962199cc0188b1748436f354f1d194a
Larry Botha <larry@fixate.it>
2020-11-05 23:42:39 -0500

    add notes on pointer

--- 08-dependency-injection/di_test.go
+++ 08-dependency-injection/di_test.go
@@ -11,6 +11,17 @@ func TestGreet(t *testing.T) {
 		buffer := bytes.Buffer{}
 
 		// why is buffer passed in with an &?
+		// https://stackoverflow.com/questions/23454940/getting-bytes-buffer-does-not-implement-io-writer-error-message/53263087#53263087
+		// https://www.geeksforgeeks.org/pointers-in-golang/
+		/*
+			if an address is not provided here, we get the following error:
+
+			bytes.Buffer does not implement io.Writer (Write method has pointer receiver)
+
+			This happens because bytes.Buffer implements Write from io.Writer with a pointer
+			receiver - when an implementation of a method uses a pointer received, that value
+			must be pass as a pointer
+		*/
 		Greet(&buffer, "Joe")
 
 		got := buffer.String()
