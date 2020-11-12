ee0813df25fe2194e09efe8255b47cf4da1c56c8
Larry Botha <larry@fixate.it>
2020-11-11 22:13:49 -0500

    begin work on goroutine

--- 10-concurrency/concurrency.go
+++ 10-concurrency/concurrency.go
@@ -1,5 +1,7 @@
 package concurrency
 
+import "time"
+
 type WebsiteChecker func(string) bool
 
 func CheckWebsitesSlow(wc WebsiteChecker, urls []string) map[string]bool {
@@ -8,6 +10,29 @@ func CheckWebsitesSlow(wc WebsiteChecker, urls []string) map[string]bool {
 	for _, url := range urls {
 		results[url] = wc(url)
 	}
+
+	return results
+}
+
+func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
+	results := make(map[string]bool)
+
+	for _, url := range urls {
+		/*
+			`go` in front of a function call makes it a goroutine
+
+			We use an anonymous function, and call it immediately
+
+			We need to pass the value to the anonymous function, otherwise the only value
+			that will be received is the last url in the slice. This is the same as closures
+			that use `this` in Javascript
+		*/
+		go func(u string) {
+			results[u] = wc(u)
+		}(url)
+	}
+
+	time.Sleep(2 * time.Second)
 
 	return results
 }
--- 10-concurrency/concurrency_test.go
+++ 10-concurrency/concurrency_test.go
@@ -28,6 +28,28 @@ func TestCheckWebsitesSlow(t *testing.T) {
 	})
 }
 
+func TestCheckWebsites(t *testing.T) {
+	t.Run("checks websites", func(t *testing.T) {
+		urls := []string{
+			"http://google.com",
+			"http://blog.gypsydave5.com",
+			"waat://furhurterwe.geds",
+		}
+
+		want := map[string]bool{
+			"http://google.com":          true,
+			"http://blog.gypsydave5.com": true,
+			"waat://furhurterwe.geds":    false,
+		}
+
+		got := CheckWebsites(mockWebsiteChecker, urls)
+
+		if !reflect.DeepEqual(got, want) {
+			t.Fatalf("got %v, want %v", got, want)
+		}
+	})
+}
+
 func mockWebsiteChecker(url string) bool {
 	if url == "waat://furhurterwe.geds" {
 		return false
