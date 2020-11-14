3b761b4da40d0a300a0b2d30cf5b1a3d01e4a75b
Larry Botha <larry@fixate.it>
2020-11-14 14:57:37 -0500

    complete example by adding channel

--- 10-concurrency/concurrency.go
+++ 10-concurrency/concurrency.go
@@ -1,8 +1,21 @@
 package concurrency
 
-import "time"
-
 type WebsiteChecker func(string) bool
+
+/*
+one can use a struct with unnamed fields
+
+these fields can be accessed using the type of the field as the name
+
+e.g.
+
+myVar := struct{int,bool}{5,true}
+number := myVar.int
+*/
+type result struct {
+	string
+	bool
+}
 
 func CheckWebsitesSlow(wc WebsiteChecker, urls []string) map[string]bool {
 	results := make(map[string]bool)
@@ -17,6 +30,13 @@ func CheckWebsitesSlow(wc WebsiteChecker, urls []string) map[string]bool {
 func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
 	results := make(map[string]bool)
 
+	/*
+		create a channel using `make`
+
+		chan expects a type describing the contents of the channel
+	*/
+	resultChannel := make(chan result)
+
 	for _, url := range urls {
 		/*
 			`go` in front of a function call makes it a goroutine
@@ -28,11 +48,28 @@ func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
 			that use `this` in Javascript
 		*/
 		go func(u string) {
-			results[u] = wc(u)
+			/*
+				a channel has a value sent to it
+
+				`<-` forms part of a 'send statement'
+
+				a 'send statement' will send a value to a channel
+			*/
+			resultChannel <- result{u, wc(u)}
 		}(url)
 	}
 
-	time.Sleep(2 * time.Second)
+	for i := 0; i < len(urls); i++ {
+		/*
+			we iterate over the length of the urls, using a 'receive expression` to assign
+			the result of each value in the channel to a variable
+
+			The value from the receive expression is of the type of the channel
+		*/
+		result := <-resultChannel
+
+		results[result.string] = result.bool
+	}
 
 	return results
 }
