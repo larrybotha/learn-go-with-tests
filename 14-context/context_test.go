1797e2c68629693f0146014569971b74138f7eed
Larry Botha <larry@fixate.it>
2020-11-24 22:07:07 -0500

    allow for cancelling request

--- 14-context/context_test.go
+++ 14-context/context_test.go
@@ -11,6 +11,7 @@ import (
 type StoreSpy struct {
 	response  string
 	cancelled bool
+	t         *testing.T
 }
 
 func (s *StoreSpy) Fetch() string {
@@ -25,7 +26,8 @@ func (s *StoreSpy) Cancel() {
 func TestHandler(t *testing.T) {
 	t.Run("handles basic requests", func(t *testing.T) {
 		data := "hello, world"
-		server := Server(&StoreSpy{response: data})
+		store := &StoreSpy{response: data, t: t}
+		server := Server(store)
 
 		request := httptest.NewRequest(http.MethodGet, "/", nil)
 		response := httptest.NewRecorder()
@@ -37,24 +39,44 @@ func TestHandler(t *testing.T) {
 		if body != data {
 			t.Errorf("got %q, want %q", body, data)
 		}
+
+		store.assertWasNotCancelled()
 	})
 
 	t.Run("allows cancelling requests", func(t *testing.T) {
-		store := &StoreSpy{response: "foo"}
+		store := &StoreSpy{response: "foo", t: t}
 		server := Server(store)
 
 		request := httptest.NewRequest(http.MethodGet, "/", nil)
 
+		// Create a derived context from the request
+		// context.WithCancel returns a cancel function which can cancel the context
 		cancellingContext, cancel := context.WithCancel(request.Context())
 		time.AfterFunc(5*time.Millisecond, cancel)
+		// create a shallow copy of request, setting the cancellingContext as its
+		// context
 		request = request.WithContext(cancellingContext)
 
 		response := httptest.NewRecorder()
 
 		server.ServeHTTP(response, request)
 
-		if !store.cancelled {
-			t.Errorf("got %v, want %v", store.cancelled, true)
-		}
+		store.assertWasCancelled()
 	})
+}
+
+func (s *StoreSpy) assertWasCancelled() {
+	s.t.Helper()
+
+	if !s.cancelled {
+		s.t.Error("was not cancelled when it should have been")
+	}
+}
+
+func (s *StoreSpy) assertWasNotCancelled() {
+	s.t.Helper()
+
+	if s.cancelled {
+		s.t.Error("was cancelled when it should not have been")
+	}
 }
