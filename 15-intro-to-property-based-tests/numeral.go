edbd5521c799bddf4ab1d5c1fb726836291d62ab
Larry Botha <larry@fixate.it>
2020-12-05 01:37:21 +0300

    add slice of roma numerals

--- 15-intro-to-property-based-tests/numeral.go
+++ 15-intro-to-property-based-tests/numeral.go
@@ -1,6 +1,28 @@
 package numeral
 
-import "strings"
+import (
+	"strings"
+)
+
+type RomanNumeral struct {
+	Value  int
+	Symbol string
+}
+
+var romanNumerals = []RomanNumeral{
+	{Symbol: "M", Value: 1000},
+	{Symbol: "D", Value: 500},
+	{Symbol: "C", Value: 100},
+	{Symbol: "XC", Value: 90},
+	{Symbol: "XCIX", Value: 99},
+	{Symbol: "L", Value: 50},
+	{Symbol: "XL", Value: 40},
+	{Symbol: "X", Value: 10},
+	{Symbol: "IX", Value: 9},
+	{Symbol: "V", Value: 5},
+	{Symbol: "IV", Value: 4},
+	{Symbol: "I", Value: 1},
+}
 
 // MCMLXXXIV => 1984
 
