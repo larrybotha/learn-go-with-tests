9dfeaad7749897764fce3ca83808a17a32140655
Larry Botha <larry@fixate.it>
2020-12-05 01:37:30 +0300

    simplify loop

--- 15-intro-to-property-based-tests/numeral.go
+++ 15-intro-to-property-based-tests/numeral.go
@@ -30,22 +30,11 @@ func ConvertToRoman(n int) string {
 	var result strings.Builder
 
 	for n > 0 {
-		switch {
-		case n > 9:
-			result.WriteString("X")
-			n -= 10
-		case n > 8:
-			result.WriteString("IX")
-			n -= 9
-		case n > 4:
-			result.WriteString("V")
-			n -= 5
-		case n > 3:
-			result.WriteString("IV")
-			n -= 4
-		default:
-			result.WriteString("I")
-			n--
+		for _, numeral := range romanNumerals {
+			for n >= numeral.Value {
+				result.WriteString(numeral.Symbol)
+				n -= numeral.Value
+			}
 		}
 	}
 
