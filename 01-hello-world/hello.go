d7764124a48aa4513f1913d020a0e239e70b87f2
Larry Botha <lawrenceguy@gmail.com>
2025-04-20 20:37:19 +0200

    feat(hello world): add support for dynamic language

--- 01-hello-world/hello.go
+++ 01-hello-world/hello.go
@@ -1,13 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
+const (
+	spanish         = "Spanish"
+	french          = "French"
+	englishGreeting = "Hello, "
+	spanishGreeting = "Ola, "
+	frenchGreeting  = "Bonjour, "
+)
 
 // Hello returns a string
-func Hello(name string) string {
-	// return "Hello, " + name
-	return fmt.Sprintf("Hello, %s", name)
+func Hello(name string, language string) string {
+	greeting := getGreeting(language)
+
+	if len(strings.TrimSpace(name)) == 0 {
+		name = "World"
+	}
+
+	return greeting + name
+}
+
+func getGreeting(language string) (greeting string) {
+	switch language {
+	case spanish:
+		greeting = spanishGreeting
+	case french:
+		greeting = frenchGreeting
+	default:
+		greeting = englishGreeting
+	}
+
+	return
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 }
--- 01-hello-world/hello_test.go
+++ 01-hello-world/hello_test.go
@@ -3,10 +3,39 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Sam")
-	want := "Hello, Sam"
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Sam", "")
+		want := "Hello, Sam"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say 'Hello, world' when an empty string is provided", func(t *testing.T) {
+		got := Hello("", "")
+		want := "Hello, World"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Sam", "Spanish")
+		want := "Ola, Sam"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Sam", "French")
+		want := "Bonjour, Sam"
+
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+func assertCorrectMessage(t testing.TB, got string, want string) {
+	t.Helper() // show line in tests where failed, as opposed to line where errored
 
 	if got != want {
-		t.Errorf("got %q want %q", got, want)
+		t.Errorf("got %q, want %q", got, want)
 	}
 }
